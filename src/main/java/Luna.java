87a2d4d2034627de5068dd099f033b296456af04
ckwflash <ckwredhorse@gmail.com>
2025-08-19 16:35:46 +0800

    Replace Duke with Luna as entry point

---
+++ src/main/java/Luna.java
@@ -0,0 +1,16 @@
+/**
+ * Application entry point
+ */
+
+public class Luna {
+    public static void main(String[] args) {
+        String text =
+            "____________________________________________________________\n"
+                + " Hello, nice to meet you! I'm Luna\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again\n"
+                + "____________________________________________________________\n";
+        System.out.println(text);
+    }
+}
