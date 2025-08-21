017c1f9153cb48e4f5fd901f38dc36c75e62b213
ckwflash <ckwredhorse@gmail.com>
2025-08-21 12:22:24 +0800

    Add Task class to manage task descriptions and completion status

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+/**
+ * Task with a description and completion status.
+ */
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Task with the specified description and sets its status to not done.
+    */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone(boolean status) {
+        this.isDone = status;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+    public String taskView() {
+        return String.format("[%s] %s", this.getStatusIcon(), description);
+    }
+}
