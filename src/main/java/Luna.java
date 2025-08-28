6463149936b08e41804167cc94bb53479c5a3da7
ckwflash <ckwredhorse@gmail.com>
2025-08-29 02:02:30 +0800

    Refactor Luna class to utilize Ui class for user interactions

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -10,57 +10,43 @@ import java.util.Scanner;
  */
 public class Luna {
     private static final String DATA_FILE_PATH = "./data/duke.txt";
+    
     public static void main(String[] args) {
         ArrayList<Task> tasks = new ArrayList<>();
+        Ui ui = new Ui();
 
         // Load tasks from file at startup
         loadTasks(tasks);
 
-        String intro =
-            "____________________________________________________________\n"
-                + " Hello, nice to meet you! I'm Luna\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n";
-        String goodbye =
-            "____________________________________________________________\n"
-                + " Goodbye! Hope to see you again\n"
-                + "____________________________________________________________\n";
-        System.out.println(intro);
-
-        Scanner scanner = new Scanner(System.in);
+        ui.showWelcome();
 
         String input = "";
-        while (scanner.hasNextLine()) {
+        while (ui.hasMoreInput()) {
             try {
-                input = scanner.nextLine();
+                input = ui.readCommand();
                 if (input.equals("bye")) {
                     break;
                 } else if (input.equals("list")) {
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println((i + 1) + ". " + tasks.get(i).taskView());
-                    }
-                    System.out.println();
+                    ui.showTaskList(tasks);
                 } else {
                     String[] parts = input.split(" ", 2);
                     String command = parts[0];
                     switch (command) {
                     case "mark":
                         if (parts.length > 1 && !parts[1].isBlank()) {
-                            markCommand(parts[1], tasks, true);
+                            markCommand(parts[1], tasks, true, ui);
                             saveTasks(tasks);
                         } else {
                             throw new LunaException("Please provide a task number to mark");
                         }
-                        System.out.println();
                         break;
                     case "unmark":
                         if (parts.length > 1 && !parts[1].isBlank()) {
-                            markCommand(parts[1], tasks, false);
+                            markCommand(parts[1], tasks, false, ui);
                             saveTasks(tasks);
                         } else {
                             throw new LunaException("Please provide a task number to unmark");
                         }
-                        System.out.println();
                         break;
                     case "delete":
                         if (parts.length > 1 && !parts[1].isBlank()) {
@@ -74,11 +60,8 @@ public class Luna {
                                 throw new LunaException("Task index is out of bounds");
                             }
                             Task removed = tasks.remove(index);
-                            System.out.println("The following task has been removed:");
-                            System.out.println("  " + removed.taskView());
-                            System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                            ui.showTaskDeleted(removed, tasks.size());
                             saveTasks(tasks);
-                            System.out.println();
                         } else {
                             throw new LunaException("Please give a task number to delete");
                         }
@@ -86,19 +69,19 @@ public class Luna {
                     case "todo":
                         Task todo = new ToDoTask(parts.length > 1 ? parts[1] : "");
                         tasks.add(todo);
-                        printAddTaskMsg(todo, tasks.size());
+                        ui.showTaskAdded(todo, tasks.size());
                         saveTasks(tasks);
                         break;
                     case "deadline":
                         Task deadline = new DeadlineTask(parts.length > 1 ? parts[1] : "");
                         tasks.add(deadline);
-                        printAddTaskMsg(deadline, tasks.size());
+                        ui.showTaskAdded(deadline, tasks.size());
                         saveTasks(tasks);
                         break;
                     case "event":
                         Task event = new EventTask(parts.length > 1 ? parts[1] : "");
                         tasks.add(event);
-                        printAddTaskMsg(event, tasks.size());
+                        ui.showTaskAdded(event, tasks.size());
                         saveTasks(tasks);
                         break;
                     default:
@@ -106,13 +89,12 @@ public class Luna {
                     }
                 }
             } catch (LunaException e) {
-                System.out.println(e.getMessage());
-                System.out.println();
+                ui.showError(e.getMessage());
             }
         }
 
-        System.out.println(goodbye);
-        scanner.close();
+        ui.showGoodbye();
+        ui.close();
     }
 
     /**
@@ -222,7 +204,7 @@ public class Luna {
     /**
      * Checks if the marking is valid and does so
      */
-    private static void markCommand(String indexStr, ArrayList<Task> tasks, boolean markDone) throws LunaException {
+    private static void markCommand(String indexStr, ArrayList<Task> tasks, boolean markDone, Ui ui) throws LunaException {
         int index;
         try {
             index = Integer.parseInt(indexStr) - 1;
@@ -234,18 +216,9 @@ public class Luna {
         }
         tasks.get(index).markDone(markDone);
         if (markDone) {
-            System.out.println("Nice! This task has been marked as done:");
-            System.out.println("  " + tasks.get(index).taskView());
+            ui.showTaskMarked(tasks.get(index));
         } else {
-            System.out.println("OK, This task has been marked as not done yet:");
-            System.out.println("  " + tasks.get(index).taskView());
+            ui.showTaskUnmarked(tasks.get(index));
         }
-    }
-
-    private static void printAddTaskMsg(Task task, int count) {
-        System.out.println(" The following task has been added:");
-        System.out.println("  " + task.taskView());
-        System.out.println(" Now you have " + count + " tasks in the list.");
-        System.out.println();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Deals with interactions with the user
+ */
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    /**
+     * Shows the welcome message when the application starts
+     */
+    public void showWelcome() {
+        String intro =
+            "____________________________________________________________\n"
+                + " Hello, nice to meet you! I'm Luna\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(intro);
+    }
+
+    /**
+     * Shows the goodbye message when the application exits
+     */
+    public void showGoodbye() {
+        String goodbye =
+            "____________________________________________________________\n"
+                + " Goodbye! Hope to see you again\n"
+                + "____________________________________________________________\n";
+        System.out.println(goodbye);
+    }
+
+    /**
+     * Reads the next line of user input
+     */
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    /**
+     * Checks if there is more input available
+     */
+    public boolean hasMoreInput() {
+        return scanner.hasNextLine();
+    }
+
+    /**
+     * Shows an error message to the user
+     */
+    public void showError(String message) {
+        System.out.println(message);
+        System.out.println();
+    }
+
+    /**
+     * Shows the list of tasks to the user
+     */
+    public void showTaskList(ArrayList<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i).taskView());
+        }
+        System.out.println();
+    }
+
+    /**
+     * Shows a message when a task is added
+     */
+    public void showTaskAdded(Task task, int totalTasks) {
+        System.out.println(" The following task has been added:");
+        System.out.println("  " + task.taskView());
+        System.out.println(" Now you have " + totalTasks + " tasks in the list.");
+        System.out.println();
+    }
+
+    /**
+     * Shows a message when a task is marked as done
+     */
+    public void showTaskMarked(Task task) {
+        System.out.println("Nice! This task has been marked as done:");
+        System.out.println("  " + task.taskView());
+        System.out.println();
+    }
+
+    /**
+     * Shows a message when a task is unmarked
+     */
+    public void showTaskUnmarked(Task task) {
+        System.out.println("OK, This task has been marked as not done yet:");
+        System.out.println("  " + task.taskView());
+        System.out.println();
+    }
+
+    /**
+     * Shows a message when a task is deleted
+     */
+    public void showTaskDeleted(Task task, int totalTasks) {
+        System.out.println("The following task has been removed:");
+        System.out.println("  " + task.taskView());
+        System.out.println("Now you have " + totalTasks + " tasks in the list.");
+        System.out.println();
+    }
+
+    /**
+     * Closes the scanner
+     */
+    public void close() {
+        scanner.close();
+    }
+}
